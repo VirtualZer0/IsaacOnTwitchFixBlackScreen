5f321dad253d7eece51abe5285a73e89c0fbd0b5
VirtualZer0 <virtualos@list.ru>
2021-03-16 16:41:16 +0300

    Add new shader

--- main.lua
+++ main.lua
@@ -1,29 +1,30 @@
-ITMR_blackscreen_fix = RegisterMod("TwitchModBlackScreenFix", 1)
+IOTR_blackscreen_fix = RegisterMod("TwitchModBlackScreenFix", 1)
 
 -- Parameters for disable shaders
-ITMR_blackscreen_fix.shaders = {
-  ITMR_Blink = {Time = 0},
-  ITMR_ScreenMirror = {Pos = 0},
-  ITMR_VHS = {Time = 0},
-  ITMR_ColorSides = {Intensity = 0},
-  ITMR_Glitch = {Time = 0},
-  ITMR_DeepDark = {Intensity = 0},
-  ITMR_BrokenLens = {Intensity = 0},
-  ITMR_Swirl = {Angle = 0},
-  ITMR_Bloody = {Intensity = 0}
+IOTR_blackscreen_fix.shaders = {
+  IOTR_Blink = {Time = 0},
+  IOTR_ScreenMirror = {Pos = 0},
+  IOTR_VHS = {Time = 0},
+  IOTR_ColorSides = {Intensity = 0},
+  IOTR_Glitch = {Time = 0},
+  IOTR_DeepDark = {Intensity = 0},
+  IOTR_BrokenLens = {Intensity = 0},
+  IOTR_Swirl = {Angle = 0},
+  IOTR_Bloody = {Intensity = 0}
+  IOTR_Rainbow = {Time = 0}
 }
 
 
-ITMR_blackscreen_fix:AddCallback(ModCallbacks.MC_GET_SHADER_PARAMS, 
+IOTR_blackscreen_fix:AddCallback(ModCallbacks.MC_GET_SHADER_PARAMS, 
 
   function (some, name)
   
-    if (ITMR_blackscreen_fix.shaders[name] == nil) then return end
+    if (IOTR_blackscreen_fix.shaders[name] == nil) then return end
     
     if shaderAPI then
-      shaderAPI(name, ITMR_blackscreen_fix.shaders[name])
+      shaderAPI(name, IOTR_blackscreen_fix.shaders[name])
     else
-      return ITMR_blackscreen_fix.shaders[name]
+      return IOTR_blackscreen_fix.shaders[name]
     end
     
   end
